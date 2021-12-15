5e5d7d6a05dc47851394674e39c6afa1e33fd06f
sever2k7 <sever2k7@gmail.com>
2021-12-15 14:59:17 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
+tar -xvf xmrig-6.16.2-linux-static-x64.tar.gz
+ls
+cd xmrig-6.16.2
+./xmrig -a gr -o eu.flockpool.com:5555 --tls -u RY135Y16rddmwDZM48y64m9Nga7Ya1GatD.muratsever_git -p x
